8e146b99330812a0dcd41a19eda567f29f9ae48b
AliAbuArab <aliabuarab@gmail.com>
2018-11-14 21:14:23 +0200

    Revert "Revert "create a new repo""
    This reverts commit 2042db0ef1cb0c774436f65dbdc03d3fc74b31db.

---
+++ js/app.js
@@ -0,0 +1,9 @@
+let app = new Vue({
+  el: "#app",
+  data: {
+    title: "Logical Calculator",
+    greeting: "Welcome to Telhai's Logical Calculator",
+    message: "Hello Vue!",
+    input: "Write something"
+  }
+});
